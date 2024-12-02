a711ef8a7a3339a70eaaf422b37b724e36fc0e88
21aej417 <m0228shoei@gmail.com>
2024-12-02 15:37:21 +0900

    TEST

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -102,6 +102,7 @@ async function seedRevenue() {
 }
 
 export async function GET() {
+  // No Error - TEST
   // return Response.json({
   //   message:
   //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
